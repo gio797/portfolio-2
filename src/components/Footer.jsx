5736ee93ed243b5054420f5a9b5f592369c5503c
gio797 <119562363+gio797@users.noreply.github.com>
2023-06-17 13:13:30 +0400

    add menu hamburger

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,25 +6,25 @@ function Footer() {
       <a href="mailto:g.muchaidze@yahoo.com" className="footer__link">
         g.muchaidze@yahoo.com
       </a>
-      <ul class="social-list">
-        <li class="social-list__item">
-          <a class="social-list__link" href="https://codepen.io">
-            <i class="fab fa-codepen"></i>
+      <ul className="social-list">
+        <li className="social-list__item">
+          <a className="social-list__link" href="https://codepen.io">
+            <i className="fab fa-codepen"></i>
           </a>
         </li>
-        <li class="social-list__item">
-          <a class="social-list__link" href="http://dribbble.com">
-            <i class="fab fa-dribbble"></i>
+        <li className="social-list__item">
+          <a className="social-list__link" href="http://dribbble.com">
+            <i className="fab fa-dribbble"></i>
           </a>
         </li>
-        <li class="social-list__item">
-          <a class="social-list__link" href="https://twitter.com">
-            <i class="fab fa-twitter"></i>
+        <li className="social-list__item">
+          <a className="social-list__link" href="https://twitter.com">
+            <i className="fab fa-twitter"></i>
           </a>
         </li>
-        <li class="social-list__item">
-          <a class="social-list__link" href="https://github.com">
-            <i class="fab fa-github"></i>
+        <li className="social-list__item">
+          <a className="social-list__link" href="https://github.com">
+            <i className="fab fa-github"></i>
           </a>
         </li>
       </ul>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/devjane.png";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <header>
       <div className="logo">
         <img src={logo} alt="" />
       </div>
-      <button className="nav-toggle" aria-label="toggle navigation">
-        <span className="hamburger"></span>
+      <button
+        className={showMenu ? "nav-toggle active" : "nav-toggle"}
+        aria-label="toggle navigation"
+        onClick={() => setShowMenu((prev) => !prev)}
+      >
+        <span className={showMenu ? "hamburger active" : "hamburger"}></span>
       </button>
-      <nav className="nav">
+      <nav className={showMenu ? "nav active" : "nav"}>
         <ul className="nav__list">
           <li className="nav__item">
-            <a href="#home" className="nav__link">
+            <a
+              href="#home"
+              className="nav__link"
+              onClick={() => setShowMenu(false)}
+            >
               Home
             </a>
           </li>
           <li className="nav__item">
-            <a href="#services" className="nav__link">
+            <a
+              href="#services"
+              className="nav__link"
+              onClick={() => setShowMenu(false)}
+            >
               My Services
             </a>
           </li>
           <li className="nav__item">
-            <a href="#about" className="nav__link">
+            <a
+              href="#about"
+              className="nav__link"
+              onClick={() => setShowMenu(false)}
+            >
               About me
             </a>
           </li>
           <li className="nav__item">
-            <a href="#work" className="nav__link">
+            <a
+              href="#work"
+              className="nav__link"
+              onClick={() => setShowMenu(false)}
+            >
               My Work
             </a>
           </li>
